b98bd156a9d257b9fe7298a8648c940cf351ed07
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-16 12:11:05 +0200

    Create _02_BasicStackOperations.java

---
+++ _01_StacksAndQueues/_02_Exercise/_02_BasicStackOperations.java
@@ -0,0 +1,32 @@
+package _01_StacksAndQueues._02_Exercise;
+
+import java.util.*;
+
+public class _02_BasicStackOperations {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int count = 1; count <= n; count++) {
+            stack.push(scanner.nextInt());
+        }
+
+        for (int count = 1; count <= s; count++) {
+            stack.pop();
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println(0); 
+        } else if (stack.contains(x)) {
+            System.out.println(true); 
+        } else {
+            System.out.println(Collections.min(stack)); 
+        }
+    }
+}
