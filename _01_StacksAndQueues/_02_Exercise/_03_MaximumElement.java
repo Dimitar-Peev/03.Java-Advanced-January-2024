8cecd1dab6fb07e5b6205577b781defe2328fc3b
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-16 21:02:16 +0200

    Update _03_MaximumElement.java

--- _01_StacksAndQueues/_02_Exercise/_03_MaximumElement.java
+++ _01_StacksAndQueues/_02_Exercise/_03_MaximumElement.java
@@ -1,31 +1,41 @@
+package _01_StacksAndQueues._02_Exercise;
+
 import java.util.*;
 
-public class _03_MaximumElement {
+public class _03_MaximumElement_v2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        Deque<Integer> numbersStack = new ArrayDeque<>();
+        ArrayDeque<Integer> numbersStack = new ArrayDeque<>();
 
-        for (int i = 0; i < n; i++) {
-            int[] input = Arrays.stream(scanner.nextLine().split(" "))
+        for (int i = 0; i < n; i++) { 
+            int[] line = Arrays.stream(scanner.nextLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
 
-            switch (input[0]) {
+            switch (line[0]) {
                 case 1:
-                    numbersStack.push(input[1]);
+                    numbersStack.push(line[1]); 
                     break;
                 case 2:
                     if (!numbersStack.isEmpty()) {
-                        numbersStack.pop();
+                        numbersStack.pop(); 
                     }
                     break;
                 case 3:
-                    System.out.println(Collections.max(numbersStack));
+                    System.out.println(getMaxElement(numbersStack));
                     break;
             }
         }
+    }
+
+    public static int getMaxElement(ArrayDeque<Integer> stack) {
+        int maxNum = Integer.MIN_VALUE;
+        for (Integer integer : stack) {
+            maxNum = (integer > maxNum) ? integer : maxNum;
+        }
+        return maxNum;
     }
 }
