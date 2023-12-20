9b864319ddd4584bba4984fdc9be6ee03516904e
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-20 15:49:44 +0200

    Create _07_CitiesByContinentAndCountry.java

---
+++ _03_SetsAndMapsAdvanced/_01_Lab/_07_CitiesByContinentAndCountry.java
@@ -0,0 +1,32 @@
+package _03_SetsAndMapsAdvanced._01_Lab;
+
+import java.util.*;
+
+public class _07_CitiesByContinentAndCountry {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Map<String, Map<String, List<String>>> info = new LinkedHashMap<>();
+
+        while (n-- > 0) {
+            String[] inputArr = scanner.nextLine().split(" ");
+            String continent = inputArr[0];
+            String county = inputArr[1];
+            String city = inputArr[2];
+
+            info.putIfAbsent(continent, new LinkedHashMap<>());
+            info.get(continent).putIfAbsent(county, new ArrayList<>());
+            info.get(continent).get(county).add(city);
+        }
+
+        info.forEach((key, value) -> {
+            System.out.println(key + ":");
+            value.forEach((k, v) -> {
+                System.out.printf("  %s -> %s%n", k, String.join(", ", v));
+            });
+        });
+    }
+}
