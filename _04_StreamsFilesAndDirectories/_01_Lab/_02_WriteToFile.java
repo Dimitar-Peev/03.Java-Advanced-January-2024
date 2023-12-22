f020711325590b9a2389894197c2806419b1ab57
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-22 12:35:55 +0200

    Create _02_WriteToFile.java

---
+++ _04_StreamsFilesAndDirectories/_01_Lab/_02_WriteToFile.java
@@ -0,0 +1,40 @@
+package _04_StreamsFilesAndDirectories._01_Lab;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _02_WriteToFile {
+    public static void main(String[] args) throws IOException {
+
+        String path = "input.txt";
+
+        FileInputStream inputStream = new FileInputStream(path);
+
+        FileOutputStream outputStream = new FileOutputStream("02.WriteToFileOutput.txt");
+
+        Set<Character> punctuations = new HashSet<>();
+        punctuations.add(',');
+        punctuations.add('.');
+        punctuations.add('!');
+        punctuations.add('?');
+
+        int singleByte = inputStream.read();
+
+        while (singleByte >= 0) {
+            char symbol = (char) singleByte;
+
+            if (!punctuations.contains(symbol)) {
+                 outputStream.write(symbol);
+            }
+
+            singleByte = inputStream.read();
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
