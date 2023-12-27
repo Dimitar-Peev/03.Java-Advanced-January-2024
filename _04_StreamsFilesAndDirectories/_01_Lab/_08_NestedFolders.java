c4e30f405f14fa9b195b1088ca4f8e99b8d327ae
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-27 11:51:38 +0200

    Create _08_NestedFolders.java

---
+++ _04_StreamsFilesAndDirectories/_01_Lab/_08_NestedFolders.java
@@ -0,0 +1,31 @@
+package _04_StreamsFilesAndDirectories._01_Lab;
+
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class _08_NestedFolders {
+    public static void main(String[] args) {
+
+        File folder = new File("Files-and-Streams");
+
+        Queue<File> dirs = new ArrayDeque<>();
+        dirs.offer(folder);
+
+        int counter = 0;
+
+        while (!dirs.isEmpty()) {
+            File current = dirs.poll();
+            File[] nestedFiles = current.listFiles();
+            for (File nestedFile : nestedFiles) {
+                if (nestedFile.isDirectory()) {
+                    dirs.offer(nestedFile);
+                }
+            }
+            counter++;
+            System.out.println(current.getName());
+        }
+
+        System.out.println(counter + " folders");
+    }
+}
