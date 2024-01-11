c406127774df034bbf83e35e61652791f49c0e2b
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-11 21:00:55 +0200

    Update _06_RecursiveFibonacci.java

--- _01_StacksAndQueues/_02_Exercise/_06_RecursiveFibonacci.java
+++ _01_StacksAndQueues/_02_Exercise/_06_RecursiveFibonacci.java
@@ -1,19 +1,15 @@
 package _01_StacksAndQueues._02_Exercise;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 public class _06_RecursiveFibonacci {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        Deque<Long> stack = new ArrayDeque<>();
-        stack.push(0L);
-        stack.push(1L);
+        Deque<Long> stack = new ArrayDeque<>(Arrays.asList(1L, 0L)) ;
 
-        int n = Integer.parseInt(scanner.nextLine());
+        int n = scanner.nextInt();
 
         if (n < 2){
             System.out.println(1);
@@ -25,10 +21,8 @@ public class _06_RecursiveFibonacci {
             long num2 = stack.pop();
 
             stack.push(num1);
-
             stack.push(num1 + num2);
         }
-      
         System.out.println(stack.pop());
     }
 }
