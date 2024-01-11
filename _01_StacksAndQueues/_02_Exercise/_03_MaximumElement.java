32a3c25a8b746d48a594c3a54cf2af5640a5aeb2
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-11 19:52:28 +0200

    Update _03_MaximumElement.java

--- _01_StacksAndQueues/_02_Exercise/_03_MaximumElement.java
+++ _01_StacksAndQueues/_02_Exercise/_03_MaximumElement.java
@@ -1,37 +1,40 @@
 package _01_StacksAndQueues._02_Exercise;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
 
-public class _03_MaximumElement_v2 {
+public class _03_MaximumElement {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        ArrayDeque<Integer> numbersStack = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < n; i++) { 
-            int[] line = Arrays.stream(scanner.nextLine().split(" "))
+        for (int i = 0; i < n; i++) {
+            int[] inputArr = Arrays.stream(scanner.nextLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
 
-            switch (line[0]) {
+            switch (inputArr[0]) {
                 case 1:
-                    numbersStack.push(line[1]); 
+                    stack.push(inputArr[1]);
                     break;
                 case 2:
-                    if (!numbersStack.isEmpty()) {
-                        numbersStack.pop(); 
+                    if (!stack.isEmpty()) {
+                        stack.pop();
                     }
                     break;
                 case 3:
-                    System.out.println(getMaxElement(numbersStack));
+                    System.out.println(getMaxElement(stack));
                     break;
             }
         }
     }
 
-    public static int getMaxElement(ArrayDeque<Integer> stack) {
+    public static int getMaxElement(Deque<Integer> stack) {
         int maxNum = Integer.MIN_VALUE;
         for (Integer integer : stack) {
             maxNum = (integer > maxNum) ? integer : maxNum;
