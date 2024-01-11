b899b772721145b49f9319a70ddab0bce92adc4c
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-11 20:05:31 +0200

    Update _05_BalancedParentheses.java

--- _01_StacksAndQueues/_02_Exercise/_05_BalancedParentheses.java
+++ _01_StacksAndQueues/_02_Exercise/_05_BalancedParentheses.java
@@ -44,3 +44,44 @@ public class _05_BalancedParentheses {
 
     }
 }
+package _01_StacksAndQueues._02_Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class _05_BalancedParentheses {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Deque<Character> openBrackets = new ArrayDeque<>();
+
+        String brackets = "({[";
+
+        boolean areBalanced = true;
+
+        for (char currentBracket : input.toCharArray()) {
+            if (brackets.contains(currentBracket + "")) {
+                openBrackets.push(currentBracket);
+            } else {
+                if (openBrackets.isEmpty()) {
+                    areBalanced = false;
+                    break;
+                }
+                char top = openBrackets.pop();
+
+                if (!(top == '(' && currentBracket == ')' 
+                        || top == '{' && currentBracket == '}'
+                        || top == '[' && currentBracket == ']')) {
+                    areBalanced = false;
+                    break;
+                }
+            }
+        }
+        String result = areBalanced ? "YES" : "NO";
+        System.out.println(result);
+    }
+}
