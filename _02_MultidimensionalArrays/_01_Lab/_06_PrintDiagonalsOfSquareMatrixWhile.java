cc7d4ff28b9515180c3434d36368f57d8917342b
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-12 21:17:13 +0200

    Update _06_PrintDiagonalsOfSquareMatrixWhile.java

--- _02_MultidimensionalArrays/_01_Lab/_06_PrintDiagonalsOfSquareMatrixWhile.java
+++ _02_MultidimensionalArrays/_01_Lab/_06_PrintDiagonalsOfSquareMatrixWhile.java
@@ -3,52 +3,49 @@ package _02_MultidimensionalArrays._01_Lab;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class _06_PrintDiagonalsOfSquareMatrixWhile {
+public class PrintDiagonalsOfSquareMatrix {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int[][] matrix = readMatrix(scanner);
 
-        first(matrix);
-        second(matrix);
+        print(matrix);
 
     }
 
-    public static int[][] readMatrix(Scanner scanner) {
+    private static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(matrix[i][i] + " ");
+        }
+
+        System.out.println();
+
+        int lastRowIndex = matrix.length - 1;
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(matrix[lastRowIndex - i][i] + " ");
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+
         int size = Integer.parseInt(scanner.nextLine());
 
         int[][] matrix = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            matrix[row] = readArray(scanner.nextLine());
+
+        for (int r = 0; r < size; r++) {
+            matrix[r] = readArray(scanner);
         }
+
         return matrix;
     }
 
-    public static int[] readArray(String line) {
-        return Arrays.stream(line.split("\\s+"))
+    private static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
     }
-    public static void second(int[][] matrix) {
-        int row = matrix.length - 1;
-        int col = 0;
 
-        while (row >= 0 && col < matrix.length) {
-            System.out.print(matrix[row][col] + " ");
-            row--;
-            col++;
-        }
-        System.out.println();
-    }
-
-    public static void first(int[][] matrix) {
-        int row = 0, col = 0;
-        while (row < matrix.length && col < matrix.length) {
-            System.out.print(matrix[row][col] + " ");
-            row++;
-            col++;
-        }
-        System.out.println();
-    }
 }
