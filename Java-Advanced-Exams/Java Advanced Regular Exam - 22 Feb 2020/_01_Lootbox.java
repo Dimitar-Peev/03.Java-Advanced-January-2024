565d8d59b9d41c62ce49d8fc177354a7be29b4d5
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-17 17:37:41 +0200

    Create _01_Lootbox.java

---
+++ Java-Advanced-Exams/Java Advanced Regular Exam - 22 Feb 2020/_01_Lootbox.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class _01_Lootbox {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] firstLine = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Queue<Integer> firstLootBox = new ArrayDeque<>();
+
+        for (int num : firstLine) {
+            firstLootBox.offer(num);
+        }
+//------------------------------------------------------------------------------------------------------------------
+        int[] secondLine = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Deque<Integer> secondLootBox = new ArrayDeque<>();
+
+        for (int num : secondLine) {
+            secondLootBox.push(num);
+        }
+//------------------------------------------------------------------------------------------------------------------
+        int collectedItems = 0;
+        while (!firstLootBox.isEmpty() && !secondLootBox.isEmpty()) {
+            int item = firstLootBox.peek() + secondLootBox.peek();
+
+            if (item % 2 == 0) {
+                collectedItems += item;
+                firstLootBox.poll();
+                secondLootBox.pop();
+            } else {
+                firstLootBox.offer(secondLootBox.pop());
+            }
+        }
+
+
+        if (firstLootBox.isEmpty()) {
+            System.out.println("First lootbox is empty");
+        } else {
+            System.out.println("Second lootbox is empty");
+        }
+
+        if (collectedItems >= 100) {
+            System.out.println("Your loot was epic! Value: " + collectedItems);
+        } else {
+            System.out.println("Your loot was poor... Value: " + collectedItems);
+        }
+    }
+}
