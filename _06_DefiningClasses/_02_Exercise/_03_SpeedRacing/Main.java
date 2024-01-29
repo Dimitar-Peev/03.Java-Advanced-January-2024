1f3f5015791c9e2dd5f1a4d8b4da54a1f3ea3355
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2024-01-29 20:06:44 +0200

    create _03_SpeedRacing

---
+++ _06_DefiningClasses/_02_Exercise/_03_SpeedRacing/Main.java
@@ -0,0 +1,44 @@
+package _06_DefiningClasses._02_Exercise._03_SpeedRacing;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String name = tokens[0];
+            double fuelAmount = Double.parseDouble(tokens[1]);
+            double fuelCost = Double.parseDouble(tokens[2]);
+
+            Car car = new Car(name, fuelAmount, fuelCost);
+            cars.add(car);
+        }
+
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+            String[] inputArr = input.split("\\s+");
+            String carModel = inputArr[1];
+            int amountOfKm = Integer.parseInt(inputArr[2]);
+
+            Car car = cars.stream().filter(c -> c.getModel().equals(carModel))
+                    .findFirst()
+                    .orElse(null);
+
+            if (car != null && car.isFuelSufficient(amountOfKm)) {
+                car.reduceFuel(amountOfKm);
+                car.setDistanceTraveled(car.getDistanceTraveled() + amountOfKm);
+            } else {
+                System.out.println("Insufficient fuel for the drive");
+            }
+
+            input = scanner.nextLine();
+        }
+        cars.forEach(System.out::println);
+    }
+}
